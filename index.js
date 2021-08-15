e5f420ec0d80b711bb33503dd587633f383ba6d5
jmarshak82 <79384476+jmarshak82@users.noreply.github.com>
2021-08-14 22:29:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.btn01').click(function () {
+    $('.side-slide').animate({ left: "0px" }, 200);
+});
+
+$('h3.nav01').click(function () {
+    $('.side-slide').animate({ left: "-285px" }, 200);
+});
